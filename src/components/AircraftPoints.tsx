a069d0322f765297866cbf7f5361f627e9c10624
derek-cook <dcook693@gmail.com>
2026-02-04 11:41:09 -0800

    added model graphics, set true track orientation

--- src/components/AircraftPoints.tsx
+++ src/components/AircraftPoints.tsx
@@ -45,6 +45,21 @@ export function AircraftPoints() {
         outlineColor: Cesium.Color.BLACK,
         outlineWidth: 1,
       });
+      entity.model = new Cesium.ModelGraphics({
+        uri: "/cesium/Assets/Cesium_Air.glb",
+        maximumScale: 20000,
+        // minimumPixelSize: 128,
+      });
+
+      // true track is in decimal degrees from 0 degrees north
+      // Subtract 90° because the model faces east by default in ENU frame
+      const heading = Cesium.Math.toRadians((ac.trueTrack ?? 0) - 90);
+      const pitch = 0;
+      const roll = 0;
+      const hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+      entity.orientation = new Cesium.ConstantProperty(
+        Cesium.Transforms.headingPitchRollQuaternion(position, hpr)
+      );
     }
 
     // Remove entities for aircraft no longer in the list
